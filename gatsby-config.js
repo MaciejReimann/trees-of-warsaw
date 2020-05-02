f6d0d6e87db81399f0060b5d7bc6b7d09c147703
Maciej Reimann <maciej.reimann@gmail.com>
2020-05-02 23:48:41 +0200

    netlify serverless function + proxying works

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const { createProxyMiddleware } = require("http-proxy-middleware")
+
 module.exports = {
   siteMetadata: {
     title: "Full Stack Bootcamp",
@@ -20,21 +22,16 @@ module.exports = {
       },
     },
     "gatsby-transformer-remark",
-    // {
-    //   resolve: `gatsby-plugin-react-css-modules`,
-    //   options: {
-    //     // *.css files are included by default.
-    //     // To support another syntax (e.g. SCSS),
-    //     // add `postcss-scss` to your project's devDependencies
-    //     // and add the following option here:
-    //     filetypes: {
-    //       ".scss": { syntax: `postcss-scss` },
-    //     },
-
-    //     // Exclude global styles from the plugin using a RegExp:
-    //     exclude: `\/global\/`,
-    //     // For all the options check babel-plugin-react-css-modules README link provided above
-    //   },
-    // },
   ],
+  developMiddleware: app => {
+    app.use(
+      "/.netlify/functions/",
+      createProxyMiddleware({
+        target: "http://localhost:9000",
+        pathRewrite: {
+          "/.netlify/functions/": "",
+        },
+      })
+    )
+  },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,37 @@
-import React from "react"
+import React, { useEffect } from "react"
+import axios from "axios"
 import { Link } from "gatsby"
 
 import Footer from "../components/footer"
 
+const apiWAW =
+  "https://api.um.warszawa.pl/api/action/datastore_search/?resource_id=ed6217dd-c8d0-4f7b-8bed-3b7eb81a95ba&limit=5"
+
+const requestConfig = {
+  url: apiWAW,
+  method: "get",
+  headers: {},
+}
+
 const IndexPage = () => {
+  const handleCick = () => {
+    axios("/.netlify/functions/hello").then(response => console.log(response))
+  }
+
+  // axios(requestConfig)
+  //   .then(function (response) {
+  //     // handle success
+  //     console.log(response)
+  //   })
+  //   .catch(function (error) {
+  //     // handle error
+  //     console.log(error)
+  //   })
+  //   .then(function () {
+  //     // always executed
+  //   })
+
+  console.log("dupa")
   return (
     <div>
       <h1>Hello.</h1>
@@ -14,6 +42,7 @@ const IndexPage = () => {
       <p>
         Need a developer? <Link to="/contact">Contact me</Link>
       </p>
+      <button onClick={handleCick}>Call Api</button>
       <Footer />
     </div>
   )
