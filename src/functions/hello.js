3df6810b8b883e1e00eefcbc7a5cda1fc6cb6dee
Maciej Reimann <maciej.reimann@gmail.com>
2020-05-03 00:28:08 +0200

    data successfully reaches fe

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,7 +1,20 @@
+const axios = require("axios")
+
 exports.handler = function (event, context, callback) {
   console.log("queryStringParameters", event.queryStringParameters)
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World",
+
+  axios({
+    url:
+      "https://api.um.warszawa.pl/api/action/datastore_search/?resource_id=ed6217dd-c8d0-4f7b-8bed-3b7eb81a95ba&limit=5",
+    method: "get",
+    headers: {},
+  }).then(response => {
+    // console.log("**************", )
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    })
   })
+
+  // console.log("**************", data)
 }
